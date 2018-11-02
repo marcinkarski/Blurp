61a8aad7fec2b9cc77ac5228f6a41dcbc610c20e
Marcin Karski <karski80@gmail.com>
2018-11-02 21:31:42 +0000

    Working blur effect

--- Blurp/ViewController.swift
+++ Blurp/ViewController.swift
@@ -2,18 +2,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupView()
-    }
+    let animator = UIViewPropertyAnimator(duration: 0.5, curve: .linear)
+    let effectView = UIVisualEffectView(effect: nil)
     
-    fileprivate func setupView() {
+    let imageView: UIImageView = {
         let image = UIImage(named: "img5")
         let imageView = UIImageView(image: image)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupBlurEffect()
+        animator.addAnimations {
+            self.effectView.effect = UIBlurEffect(style: .regular)
+        }
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(hangleGesture)))
+    }
+    
+    fileprivate func setupView() {
         view.addSubview(imageView)
-        NSLayoutConstraint.activate([imageView.leftAnchor.constraint(equalTo: view.leftAnchor), imageView.topAnchor.constraint(equalTo: view.topAnchor), imageView.rightAnchor.constraint(equalTo: view.rightAnchor), imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+        NSLayoutConstraint.activate([imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor), imageView.topAnchor.constraint(equalTo: view.topAnchor), imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor), imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+    }
+    
+    fileprivate func setupBlurEffect() {
+        view.addSubview(effectView)
+        effectView.frame = view.frame
+    }
+    
+    @objc fileprivate func hangleGesture(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        let translatedCenterY = view.center.y + translation.y
+        let progress = translatedCenterY / self.view.bounds.size.height
+        animator.fractionComplete = progress
+        print(progress)
     }
 }
